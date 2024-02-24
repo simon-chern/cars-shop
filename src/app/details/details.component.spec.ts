ab88b0588cdf3bad8336796efd47d6ae18a9e89e
simon-chern <148482820+simon-chern@users.noreply.github.com>
2024-02-24 22:49:06 +0300

    submit test

--- src/app/details/details.component.spec.ts
+++ src/app/details/details.component.spec.ts
@@ -2,18 +2,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DetailsComponent } from './details.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { CarsService } from '../cars.service';
+import { CarComponent } from '../car/car.component';
+
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
+  let mockActivatedRoute: Partial<ActivatedRoute>;
+  let mockCarsService: Partial<CarsService>;
+
 
   beforeEach(async () => {
+    mockCarsService = {
+      submitForm: jasmine.createSpy('submitForm')
+    };
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientModule],
-      providers: [DetailsComponent]
+      providers: [DetailsComponent],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +31,17 @@ describe('DetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should submit the form and reset it on submitForm', () => {
+    // Mock form data
+    component.carForm.setValue({ name: 'John', surname: 'Doe', phone: '123456789' });
+
+    // Trigger submitForm
+    component.submitForm();
+
+    // Expect submitForm to be called with the form data
+    //expect(mockCarsService.submitForm).toHaveBeenCalledWith('John', 'Doe', '123456789');
+
+    expect(component.carForm.value).toEqual({ name: null, surname: null, phone: null });
   });
 });
