94b4eca5b4d50ab162247484ac6a17f8ff938edc
simon-chern <148482820+simon-chern@users.noreply.github.com>
2024-04-30 20:17:04 +0300

    security change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,17 +4,9 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { firebaseConfig } from '../../apikey';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCD3xNtgretbCL6K6zSfIJ7QlVQgSzedp8",
-  authDomain: "cars-6fc32.firebaseapp.com",
-  projectId: "cars-6fc32",
-  storageBucket: "cars-6fc32.appspot.com",
-  messagingSenderId: "8518349863",
-  appId: "1:8518349863:web:d92b7ebb5177168978ca02",
-  measurementId: "G-NKSZWH5R1V"
-};
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
